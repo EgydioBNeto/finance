d2eea103dcb19fd8fe336c2f5a687b0f74fb5a09
EgydioBNeto <egydiobolonhezi@gmail.com>
2023-04-09 18:55:00 -0300

    add dbConnect.js

---
+++ src/config/dbConnect.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Load environment variables
+dotenv.config();
+
+// Get the MongoDB connection string based on the environment
+const connectionString = process.env.MONGODB_URL_PROD;
+
+// Connect to the MongoDB database
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Get the database connection object
+const db = mongoose.connection;
+
+// Handle errors that may occur during the database connection
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+// Export the database connection object
+export default db;
